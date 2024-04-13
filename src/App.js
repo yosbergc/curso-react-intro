3d410feb304cc52695c16828521d4cc52e187035
Yosber Guerrero <yosbergc@gmail.com>
2024-04-12 22:21:58 -0400

    Added my first useEffect for loading & error states in the communication with the localStorage

--- src/App.js
+++ src/App.js
@@ -4,35 +4,48 @@ import { TodoList } from './components/TodoList';
 import { SingleTodo } from './components/SingleTodo';
 import { AddTodoBtn } from './components/AddTodoBtn';
 import { AddNewTodoModal } from './components/AddNewTodoModal';
+import { LoadingSpinner } from './components/LoadingSpinner';
 import React from 'react';
 import './App.css'
 
-function useLocalStorage() {
-    const taskDB = localStorage.getItem('TasksDB');
-    let taskState;
-    if (!taskDB) {
-        localStorage.setItem('TasksDB', JSON.stringify([]));
-        taskState = JSON.parse(taskDB)
-    } else {
-        taskState = JSON.parse(taskDB)
-    }
-    let [item, setItem] = React.useState(taskState)
+function useLocalStorage(initialValue) {
+    let [item, setItem] = React.useState(initialValue)
+    let [loading, setLoading] = React.useState(true);
+    let [error, setError] = React.useState(false);
+
+    React.useEffect(() => {
+        const taskDB = localStorage.getItem('TasksDB');
+        let taskState;
+
+        if (!taskDB) {
+            localStorage.setItem('TasksDB', JSON.stringify([]));
+            taskState = JSON.parse(taskDB)
+        } else {
+            taskState = JSON.parse(taskDB)
+        }
+        setItem(taskState)
+        // setLoading(false);
+    }, [])
 
     function updateTodoDB(newTodoList) {
         setItem(newTodoList)
         localStorage.setItem('TasksDB', JSON.stringify(newTodoList))
     }
-    return [item, updateTodoDB];
+    return {
+        item,
+        updateTodoDB,
+        loading,
+        error
+    };
 }
 function App() {
-    let [todoSingle, updateTodoDB] = useLocalStorage()
+    let {item: todoSingle, updateTodoDB, loading, error} = useLocalStorage([])
     let [AddTodoState, setAddTodoState] = React.useState('');
     let [modalAddTodo, setModalAddTodo] = React.useState(false);
     let [newTodoState, setNewTodoState] = React.useState('');
     let totalCompleted = todoSingle.filter(single => (single.terminado)).length;
     let totalSingle = todoSingle.length;
     let todoFilter = todoSingle.filter(todo => todo.texto.toLowerCase().includes(AddTodoState.toLowerCase()));
-
     function showModal() {
         let newState = !modalAddTodo;
         setModalAddTodo(newState)
@@ -68,6 +81,9 @@ function App() {
         />
         </section>
         <TodoList>
+            {loading && <LoadingSpinner/>}
+            {error && <p>Hay un error</p>}
+            {(!loading && todoFilter.length === 0) && <p>Agrega tu tarea</p>}
             {todoFilter.map(elemento => (
             <SingleTodo
             texto={elemento.texto}
