1c1ec31fdc39bc3f1dc81264eee04dcc1e153b18
Yosber Guerrero <yosbergc@gmail.com>
2024-04-14 20:19:19 -0400

    Added Portal to the modal component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,11 +1,11 @@
 import './addNewTodoModal.css'
 import React from 'react'
+import ReactDOM from 'react-dom';
 import { AppContext } from '../AppContext'
 function Modal({children}) {
     let {showModal: onToggle} = React.useContext(AppContext)
-    return (<section className={`modal`}>
-        <p className='closeModal' onClick={onToggle}>X</p>
-        {children}
-    </section>)
-}
+    return (ReactDOM.createPortal(<section className={`modal`}>
+    <p className='closeModal' onClick={onToggle}>X</p>
+    {children}
+</section>, document.getElementById('modalContainer')))}
 export {Modal}
